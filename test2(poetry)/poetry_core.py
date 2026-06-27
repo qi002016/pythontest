e156fc7e9b5be5da716ada80999d9c2eacb6b267
ChenChen0813 <3284862414@qq.com>
2026-06-27 16:23:06 +0800

    Changes

--- test2(poetry)/poetry_core.py
+++ test2(poetry)/poetry_core.py
@@ -95,7 +95,3 @@ class PoetryCrawler:
             return
         self.save_to_file(poem_data)
         print(f"✅ 采集完毕，本次共获取{len(poem_data)}首诗文，保存路径：{self.save_path}")
-
-if __name__ == "__main__":
-    crawler = PoetryCrawler()
-    crawler.run()
